25739657b2ba2f9392a1676a2fd228026c4215c0
unknown <ericking2501@gmail.com>
2020-07-02 01:07:23 -0600

    v1.0

---
+++ App.js
@@ -0,0 +1,80 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+import Inicio from './views/Inicio';
+import NuevoCliente from './views/NuevoCliente';
+import DetallesCliente from './views/DetallesCliente';
+
+const Stack = createStackNavigator();
+
+//Definir el tema de diseño
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#1774f2',
+    accent: '#0655bf'
+  }
+}
+
+//console.log(theme.colors);
+
+
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Inicio"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: theme.colors.primary
+            },
+            headerTintColor: theme.colors.surface,
+            headerTitleStyle: {
+              fontWeight: 'bold'
+            }
+          }}
+        >
+          <Stack.Screen
+            name="Inicio"
+            component={Inicio}
+          />
+          <Stack.Screen
+            name="NuevoCliente"
+            component={NuevoCliente}
+            options={{
+              title: "Nuevo Cliente"
+            }}
+          />
+          <Stack.Screen
+            name="DetallesCliente"
+            component={DetallesCliente}
+            options={{
+              title: "Detalles Cliente"
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
